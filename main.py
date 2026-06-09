46c006bca55471c306413004d20ad08c286dcc0e
Peremil Söderström <peremilsoderstrom2@hotmail.com>
2026-06-09 08:13:21 +0200

    added comments and some light formatting

--- main.py
+++ main.py
@@ -37,11 +37,20 @@ def create_timeline (resolve, audio_file: str, black_video: str): #you need to p
 
 
     #This part adds (the black video and) the music to the timeline
+    """ 
+    I noticed when creating the timeline that it is easier to 
+    have the black placeholder video in the media pool and create 
+    in/outs and place many smaller clips on the timeline, instead of
+    adding the video to the timeline and then cut it up according to timestamps
+    """
     root = media_pool.GetRootFolder()
     clip_list = root.GetClipList()
-    #media_pool.AppendToTimeline(clip_list[1]) #the timeline is the first ([0]) object
+    #media_pool.AppendToTimeline(clip_list[1]) #the timeline is the first ([0]) object, this is black video
 
     #This is needed so the music isnt added at the end of the video
+    """
+    Might not be needed anymore, but it's very explicit and robus
+    """
     audio_clip_info = {
         "mediaPoolItem": clip_list[2],
         "startFrame": 0,
@@ -61,12 +70,17 @@ def add_cuts(resolve, beat_times):
 
     root = media_pool.GetRootFolder()
     clip_list = root.GetClipList()
-    black_video = clip_list[1]
+    black_video = clip_list[1] #the timeline is the first ([0]) object, this is the black placeholder video
 
-    offset = timeline.GetStartFrame()
+    offset = timeline.GetStartFrame() #needed to do floating point maths. the timeline starts at 1hour instead of 00:00:00:00
 
-    print(f"offset is {offset}")
+    #print(f"offset is {offset}") debug
 
+    """
+    If the first beat isnt detected at the start of the music. Like if there is an intro or
+    silence at the start. We need to place a longer placeholder video at the beginning to get the timing right.
+    The timing we get from the first timestamp from the beat detection in librosa.
+    """
     intro_clip_info = {
     "mediaPoolItem": black_video,
     "startFrame": 0,
@@ -76,6 +90,9 @@ def add_cuts(resolve, beat_times):
     }
     media_pool.AppendToTimeline([intro_clip_info])
 
+    """
+    Generate clips for every 4th beat. 
+    """
     for i in range(0, len(beat_times) - 4, 4):
         start_frame = int(beat_times[i] * fps) + offset
         end_frame = int(beat_times[i + 4] * fps) + offset
@@ -98,9 +115,12 @@ def timestamps_for_beats(file_path: str) -> list:
         y, sr = librosa.load(file_path) #open the song file
     except FileNotFoundError:
         exit("Not a valid file path")
-    
-    tempo, beatframes = librosa.beat.beat_track(y=y, sr=sr) #extracts beatframes where beat events are detected.
-                                                            # also detects bpm, but it is not useful atm
+     
+    """
+    extracts beatframes where beat events are detected.
+    also detects bpm, but it is not useful atm
+    """
+    tempo, beatframes = librosa.beat.beat_track(y=y, sr=sr) 
 
     beat_times = librosa.frames_to_time(beatframes, sr=sr) #converts beatframes to beat times, time stamps in the song.
 
@@ -115,13 +135,14 @@ def main():
     audio_file = "/Users/peremils/Documents/repos/first_uv_project/test_media/ES_Say a Prayer (Instrumental Version) - Egaoya.wav" #input(f"Enter file path for a song: ")
     black_video = "/Users/peremils/Documents/repos/first_uv_project/test_media/Black_5_minutes.mp4" #input("Enter filepath for black video: ")
 
-    timestamps = timestamps_for_beats(file_path=audio_file.strip("'").strip('"')) #must remove qutations from the strin for the filepath
+    timestamps = timestamps_for_beats(file_path=audio_file.strip("'").strip('"')) #must remove qutations from the string for the filepath
 
     resolve = connect_to_resolve()
 
     create_timeline(resolve, audio_file.strip("'").strip('"'), black_video.strip("'").strip('"'))
 
     add_cuts(resolve, timestamps)
+
 
 if __name__ == "__main__":
     main()
